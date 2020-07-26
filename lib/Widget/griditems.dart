264a703faef1db97f886e65a6b0441625e51c454
SwapnanilHalder <halderswapnanil@gmail.com>
2020-07-27 03:02:45 +0530

    2nd commit

--- lib/Widget/griditems.dart
+++ lib/Widget/griditems.dart
@@ -15,7 +15,10 @@ class GridItems extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(15),
-      child: Text(title),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.headline6,
+      ),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(15),
         gradient: LinearGradient(
