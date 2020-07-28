fbcc210c8dad2bea32b160fd8e291878a809ab02
SwapnanilHalder <halderswapnanil@gmail.com>
2020-07-28 12:54:56 +0530

    completed screen pushing via routes

--- lib/Screens/category_meals_screen.dart
+++ lib/Screens/category_meals_screen.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
-  final String categoryId;
-  final String categoryTitle;
+  static const routeName = '/category-meals';
+  // final String categoryId;
+  // final String categoryTitle;
 
-  CategoryMealsScreen({
-    @required this.categoryId,
-    @required this.categoryTitle,
-  });
+  // CategoryMealsScreen({
+  //   @required this.categoryId,
+  //   @required this.categoryTitle,
+  // });
   @override
   Widget build(BuildContext context) {
+    final Map<String, Object> routeArgs =
+        ModalRoute.of(context).settings.arguments;
+    final String categoryId = routeArgs['id'];
+    final String categoryTitle = routeArgs['title'];
     return Scaffold(
       appBar: AppBar(
-        title: Text(categoryTitle),
+        title: Text('$categoryTitle : ID: $categoryId'),
       ),
       body: Center(
         child: Text("This is the meals page of $categoryTitle"),
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/Screens/category_meals_screen.dart';
 
 import './Screens/categories_screen.dart';
 
@@ -32,6 +33,7 @@ class MyApp extends StatelessWidget {
       ),
       routes: {
         '/': (_) => CategoriesScreen(),
+        CategoryMealsScreen.routeName: (_) => CategoryMealsScreen(),
       },
     );
   }
